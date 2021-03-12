8f0deb3a3a47b2ddb927e562cc0c14a4985cc733
Marcin Syzdol <merlot@poczta.fm>
2021-03-12 12:47:02 +0100

    first message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>aplikacja</div>
+     );
+  }
+}
+ 
+export default App;
